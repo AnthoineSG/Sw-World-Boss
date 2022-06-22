3dc327798ac990ba22718ac6b3c28ac3edb34e33
AnthoineSG <anthoine93@outlook.fr>
2022-06-22 16:28:29 +0200

    reinstall pug

--- server/api/app/models/index.ts
+++ server/api/app/models/index.ts
@@ -1,10 +1,11 @@
+/* eslint-disable quotes */
 import { pool } from "./config/dbconnect";
 
 export const mainModel = {
     async allUsers() {
         const client = await pool.connect();
         const query = {
-            text: "SELECT * FROM \"wizard_info\";",
+            text: `SELECT * FROM "wizard_info";`,
         };
         const result = await client.query(query);
         return result.rows;
@@ -13,7 +14,7 @@ export const mainModel = {
     async getOneUser(id: number) {
         const client = await pool.connect();
         const query = {
-            text: "SELECT * FROM \"wizard_info\" WHERE id = $1;",
+            text: `SELECT * FROM "wizard_info" WHERE id = $1;`,
             values: [ id ]
         };
         const result = await client.query(query);
